58ed68d1ba18bf04f06324b0c1f86e7f00bb7acb
nathanielglenn02 <106645313+nathanielglenn02@users.noreply.github.com>
2024-08-30 12:23:07 +0700

    Upload IMG

--- service/config.php
+++ service/config.php
@@ -14,3 +14,37 @@ if ($koneksi->connect_error) {
 
 
 $main_url = "http://localhost/Capstone-FSP/";
+
+function uploadimg($url)
+{
+    $namafile = $_FILES['image']['name'];
+    $ukuran = $_FILES['image']['size'];
+    $error = $_FILES['image']['error'];
+    $tmp = $_FILES['image']['tmp_name'];
+
+    //cek file yang dipload 
+    $validExtension = ['jpg', 'jpeg', 'png'];
+    $fileExtension = explode('.', $namafile);
+    $fileExtension = strtolower(end($fileExtension));
+    if (!in_array($fileExtension, $validExtension)) {
+        header("location:" . $url . '?msg=notimage');
+        die;
+    }
+
+    //cek ukuran gambar
+    if ($ukuran > 1000000) {
+        header("location:" . $url . '?msg=oversize');
+        die;
+    }
+
+    //generate nama file gambar
+    if ($url == 'index.php') {
+        $namafilebaru = rand(0, 50) . '-Team' . '.' . $fileExtension;
+    } else {
+        $namafilebaru = rand(10, 1000) . '-User' . '.' . $namafile;
+    }
+
+    //upload gambar
+    move_uploaded_file($tmp, "../asset/image/" . $namafilebaru);
+    return $namafilebaru;
+}
