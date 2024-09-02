e1dd12861d42f95fa337ae52f5b873cdaecdd089
Luky20 <lukywilan@gmail.com>
2024-09-02 20:36:23 +0700

    team.php

--- class/classTeam.php
+++ class/classTeam.php
@@ -4,9 +4,11 @@ class Team
     private $teamId;
     private $teamName;
     private $gameId; // Foreign key ke Game
+    private $conn;
 
-    public function __construct($teamId, $teamName, $gameId)
+    public function __construct($conn, $teamId = null, $teamName = null, $gameId = null)
     {
+        $this->conn = $conn;
         $this->teamId = $teamId;
         $this->teamName = $teamName;
         $this->gameId = $gameId;
@@ -41,5 +43,26 @@ class Team
     public function setGameId($gameId)
     {
         $this->gameId = $gameId;
+    }
+
+    public static function getAllTeams($koneksi)
+    {
+        $query = "SELECT t.idteam, g.name as game_name, t.name as team_name FROM team as t
+                    inner join game as g on t.idgame = g.idgame";
+        $result = mysqli_query($koneksi, $query);
+
+        if (!$result) {
+            die("Query Error: " . mysqli_error($koneksi));
+        }
+
+        $teams = [];
+
+        while ($row = mysqli_fetch_assoc($result)) {
+            var_dump($row);
+            $team = new Team($row['idteam'], $row['team_name'], $row['game_name']);
+            $teams[] = $team;
+        }
+
+        return $teams;
     }
 }
--- index.php
+++ index.php
@@ -4,6 +4,9 @@ $title = 'Dashboard';
 require_once('template/header.php');
 require_once('template/sidebar.php');
 require_once('template/navbar.php');
+require_once('class/classTeam.php');
+
+$teams = Team::getAllTeams($koneksi);
 ?>
 
 
@@ -35,46 +38,22 @@ require_once('template/navbar.php');
             <table>
                 <thead>
                     <tr>
-                        <th>Team</th>
+                        <th>ID Team</th>
+                        <th>Name</th>
                         <th>Game</th>
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>
-                            <img src="img/people.png">
-                            <p>Team A</p>
-                        </td>
-                        <td>PUBG</td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <img src="img/people.png">
-                            <p>Team A</p>
-                        </td>
-                        <td>Mobile Legend</td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <img src="img/people.png">
-                            <p>Team A</p>
-                        </td>
-                        <td>Valorant</td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <img src="img/people.png">
-                            <p>Team A</p>
-                        </td>
-                        <td>Honor of King</td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <img src="img/people.png">
-                            <p>Team A</p>
-                        </td>
-                        <td>Clash Royale</td>
-                    </tr>
+                <?php
+                    // Loop untuk menampilkan data game yang diambil dari database
+                    foreach ($teams as $team) {
+                        echo "<tr>";
+                        echo "<td>" . htmlspecialchars($team->getTeamId()) . "</td>";
+                        echo "<td><p>" . htmlspecialchars($team->getTeamName()) . "</p></td>";
+                        echo "<td>" . htmlspecialchars($team->getGameId ()) . "</td>";
+                        echo "</tr>";
+                    }
+                    ?>
                 </tbody>
             </table>
         </div>
