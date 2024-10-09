5375d705c0ec3b9ec3c281c15fa5298293e57c46
fxsurya27 <s160422003@student.ubaya.ac.id>
2024-10-09 07:27:10 +0700

    up sedikit achievement

--- admin/achievement/achievement.php
+++ admin/achievement/achievement.php
@@ -28,6 +28,8 @@ $stmt->execute();
 $total_achievements_result = $stmt->get_result();
 $total_achievements = $total_achievements_result->fetch_assoc()['total'];
 $total_pages = ceil($total_achievements / $limit);
+
+$_SESSION['return_url'] = $_SERVER['REQUEST_URI'];
 ?>
 
 <main>
@@ -88,11 +90,11 @@ $total_pages = ceil($total_achievements / $limit);
             <div class="pagination" style="text-align: right;">
                 <?php
                 if ($page > 1): ?>
-                <a href="?page=<?= $page - 1 ?>&search=<?= urlencode($search) ?>">
-                    << </a>
+                    <a href="?page=<?= $page - 1 ?>&search=<?= urlencode($search) ?>">
+                        << </a>
                         <?php else: ?>
-                        <a href="#" class="disabled">
-                            << </a>
+                            <a href="#" class="disabled">
+                                << </a>
                                 <?php endif; ?>
 
                                 <?php
@@ -102,17 +104,17 @@ $total_pages = ceil($total_achievements / $limit);
                                 $end_page = min($max_hal, $start_page + 2);
 
                                 for ($hal = $start_page; $hal <= $end_page; $hal++): ?>
-                                <?php if ($hal == $page): ?>
-                                <b><?= $hal ?></b>
-                                <?php else: ?>
-                                <a href="?page=<?= $hal ?>&search=<?= urlencode($search) ?>"><?= $hal ?></a>
-                                <?php endif; ?>
+                                    <?php if ($hal == $page): ?>
+                                        <b><?= $hal ?></b>
+                                    <?php else: ?>
+                                        <a href="?page=<?= $hal ?>&search=<?= urlencode($search) ?>"><?= $hal ?></a>
+                                    <?php endif; ?>
                                 <?php endfor; ?>
 
                                 <?php if ($page < $max_hal): ?>
-                                <a href="?page=<?= $page + 1 ?>&search=<?= urlencode($search) ?>">>></a>
+                                    <a href="?page=<?= $page + 1 ?>&search=<?= urlencode($search) ?>">>></a>
                                 <?php else: ?>
-                                <a href="#" class="disabled">>></a>
+                                    <a href="#" class="disabled">>></a>
                                 <?php endif; ?>
             </div>
         </div>
